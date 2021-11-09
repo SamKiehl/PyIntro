6b6a0c6a8d962fb9a30c8acec1df821b9e4ed119
Sam Kiehl <sam.g.kiehl@gmail.com>
2021-11-08 20:24:07 -0500

    Initial Commit

---
+++ Intro.py
@@ -0,0 +1,44 @@
+# Slide 1
+print("Hello, World!")
+
+# Slide 2
+
+# Defining a string:
+words = "abc123"
+
+# Defining a number:
+e = 2.71828
+
+# Defining a boolean:
+isAlive = True
+
+# Slide 3
+
+# Making an 'if' statement:
+if(isAlive):
+    print("Yes")
+
+# Making an 'else' statement:
+else:
+    print("No")
+
+# Slide 4
+num = 3 * 4 / 2 + 10
+
+# Slide 5
+
+# This loop creates a variable, 'i', 
+# that increases by 1 each time the
+# code runs. It prints the value of i
+# each time it runs.
+for i in range(0, 10):
+    print(i)
+
+# This while loop runs while the 
+# variable x, which is initially 10, 
+# is greater than 0. Each time the loop
+# runs, the value of x is decreased by 1.
+x = 10
+while(x > 0):
+    print(x)
+    x = x - 1
